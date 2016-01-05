1ca40bc76b7fc64340e4e1334956d038b99e385e
Gasheth <sebtof@gmail.com>
2016-01-05 02:00:09 +0100

    Simple test file edit

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php 
-$conn = mysqli_connect('localhost', 'root', '', 'rg', '3307');
-if (!$conn) {
-    die('Could not connect to MySQL: ' . mysqli_connect_error());
-}
-mysqli_query($conn, 'SET NAMES \'utf8\'');
-// TODO: insert your code here.
-echo '<table class="table table-bordered table-condensed">';
-echo '<tr>';
-echo '<th>Id</th>';
-echo '<th>Prenom</th>';
-echo '<th>Nom</th>';
-echo '<th>Email</th>';
-echo '<th>Add1</th>';
-echo '</tr>';
-$result = mysqli_query($conn, 'SELECT `Id`, `Prenom`, `Nom`, `Email`, `Add1` FROM `user`');
-while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
-    echo '<tr>';
-    echo '<td>' . $row['Id'] . '</td>';
-    echo '<td>' . $row['Prenom'] . '</td>';
-    echo '<td>' . $row['Nom'] . '</td>';
-    echo '<td>' . $row['Email'] . '</td>';
-    echo '<td>' . $row['Add1'] . '</td>';
-    echo '</tr>';
-}
-mysqli_free_result($result);
-echo '</table>';
-mysqli_close($conn);
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
