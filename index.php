2a82a016a7f59a65750dd526616137c9ab41ec79
Gasheth <sebtof@gmail.com>
2016-01-05 02:15:01 +0100

    Affichage bloc jour / semaine

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>Media Calendar</title>
 
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -14,67 +14,73 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-7"> <h1>TV CALENDAR</h1></div>
+                <div class="col-md-7"> <h1>Media Calendar</h1></div>
             </div>
             <div class="row">
                 <div class="col-md-7 well well-sm"> <h1>Nav</h1></div>
             </div>
             <div class="row">
-                <div class="col-md-1"><div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
-                        </div>
-                        <div class="panel-body">
-                            Panel content
-                        </div>
-                    </div></div>
-                <div class="col-md-1"><div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
-                        </div>
-                        <div class="panel-body">
-                            Panel content
-                        </div>
-                    </div></div>
                 <div class="col-md-1"><div class="panel panel-primary">
                         <div class="panel-heading">
                             <h3 class="panel-title">4 Lundi</h3>
                         </div>
                         <div class="panel-body">
-                            Panel content
-                            <p><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg"></p>
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
                         </div>
                     </div></div>
-                <div class="col-md-1"><div class="panel panel-default">
+                  <div class="col-md-1"><div class="panel panel-primary">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
+                            <h3 class="panel-title">5 Mardi</h3>
                         </div>
                         <div class="panel-body">
-                            Panel content
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
                         </div>
                     </div></div>
-                <div class="col-md-1"><div class="panel panel-default">
+                  <div class="col-md-1"><div class="panel panel-primary">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
+                            <h3 class="panel-title">6 Mercredi</h3>
                         </div>
                         <div class="panel-body">
-                            Panel content
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
                         </div>
                     </div></div>
-                <div class="col-md-1"><div class="panel panel-default">
+                  <div class="col-md-1"><div class="panel panel-warning">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
+                            <h3 class="panel-title">7 Jeudi</h3>
                         </div>
                         <div class="panel-body">
-                            Panel content
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
                         </div>
                     </div></div>
-                <div class="col-md-1"><div class="panel panel-default">
+                  <div class="col-md-1"><div class="panel panel-primary">
                         <div class="panel-heading">
-                            <h3 class="panel-title">Panel title</h3>
+                            <h3 class="panel-title">8 Vendredi</h3>
                         </div>
                         <div class="panel-body">
-                            Panel content
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
+                        </div>
+                    </div></div>
+                  <div class="col-md-1"><div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">9 Samedi</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
+                        </div>
+                    </div></div>
+                  <div class="col-md-1"><div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">10 Dimanche</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><input type="checkbox" name="VU" value="ON" /> <strong>The Big Bang Theory</strong><img class="img-rounded img-responsive" src="http://www.pogdesign.co.uk/cat/imgs/sismall/The-Big-Bang-Theory.jpg">
+                                S: 9 - Ep: 12<br>2:00am</p>
                         </div>
                     </div></div>
             </div>
